42d8c7a0b65a47d7e8bcee90ea2c674a86e0ee90
Paweł Sobczak <pawel.sobczak0@gmail.com>
2023-03-20 21:36:59 +0100

    Basic camera mouse controls

--- src/camera.rs
+++ src/camera.rs
@@ -1,5 +1,6 @@
 use bevy::{
     core_pipeline::clear_color::ClearColorConfig,
+    input::mouse::MouseWheel,
     prelude::*,
     window::{CursorGrabMode, PrimaryWindow},
 };
@@ -10,17 +11,26 @@ use crate::GameState;
 pub struct CameraPlugin;
 
 const CAMERA_MIN_HEIGHT: f32 = 3.0;
-const CAMERA_MAX_HEIGHT: f32 = 15.0;
+const CAMERA_MAX_HEIGHT: f32 = 16.0;
 const CAMERA_START_HEIGHT: f32 = 7.0;
+const CAMERA_MOVE_SPEED: f32 = 50.0;
+
+const ZOOM_SPEED: f32 = 10.0;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_camera)
-            .add_system(restrict_camera)
-            .add_systems((restrict_camera,).in_set(OnUpdate(GameState::InGame)))
+        app.add_startup_systems((spawn_camera, set_default_cursor_position))
+            .add_systems((restrict_camera, move_camera, zoom).in_set(OnUpdate(GameState::InGame)))
             .add_system(set_cursor_as_confined.in_schedule(OnEnter(GameState::InGame)))
             .add_system(release_cursor.in_schedule(OnEnter(GameState::Menu)));
     }
+}
+
+fn set_default_cursor_position(mut window: Query<(&mut Window, With<PrimaryWindow>)>) {
+    let (mut window, _) = window.single_mut();
+    let x = window.width() / 2.0;
+    let y = window.height() / 2.0;
+    window.set_cursor_position(Some(Vec2::new(x, y)))
 }
 
 fn set_cursor_as_confined(mut window: Query<(&mut Window, With<PrimaryWindow>)>) {
@@ -33,25 +43,32 @@ fn release_cursor(mut window: Query<(&mut Window, With<PrimaryWindow>)>) {
     window.cursor.grab_mode = CursorGrabMode::None
 }
 
-// fn test_cast_ray(
-//     camera_2d: Query<(&GlobalTransform, &Collider, With<Camera3d>)>,
-//     ground: Query<(&GlobalTransform, &Collider, With<Unit>)>,
-// ) {
-//     let (camera_2d_transform, camera_collider, _) = camera_2d.single();
-//     let (ground_tranform, collider, _) = ground.single();
+fn move_camera(
+    window: Query<(&Window, With<PrimaryWindow>)>,
+    mut camera: Query<(&mut Transform, With<Camera3d>)>,
+    time: Res<Time>,
+) {
+    let (window, _) = window.single();
+    if let Some(cursor_position) = window.cursor_position() {
+        let (mut transform, _) = camera.single_mut();
 
-//     // info!("ray_origin: {}", camera_2d_transform.translation());
-//     // info!("ray_dir: {}", ground_tranform.translation());
+        if cursor_position.x >= window.width() - 1.0 {
+            transform.translation.x += CAMERA_MOVE_SPEED * time.delta_seconds()
+        }
 
-//     let xd = camera_collider.cast_local_ray(
-//         camera_2d_transform.translation(),
-//         ground_tranform.translation(),
-//         1000.0,
-//         true,
-//     );
+        if cursor_position.x <= 1.0 {
+            transform.translation.x -= CAMERA_MOVE_SPEED * time.delta_seconds()
+        }
 
-//     // info!("{:?}", xd);
-// }
+        if cursor_position.y >= window.height() - 1.0 {
+            transform.translation.z -= CAMERA_MOVE_SPEED * time.delta_seconds()
+        }
+
+        if cursor_position.y <= 1.0 {
+            transform.translation.z += CAMERA_MOVE_SPEED * time.delta_seconds()
+        }
+    }
+}
 
 fn restrict_camera(mut camera: Query<(&mut Transform, With<Camera3d>)>) {
     let (mut transform, _) = camera.single_mut();
@@ -81,7 +98,8 @@ fn spawn_camera(mut commands: Commands) {
 
     commands.spawn((
         Camera3dBundle {
-            transform: Transform::from_xyz(-2.0, 2.5, 5.0),
+            transform: Transform::from_xyz(0.0, CAMERA_START_HEIGHT, 0.0)
+                .with_rotation(Quat::from_rotation_x(-45.0_f32.to_radians())),
             camera: Camera {
                 order: 0,
                 ..default()
@@ -108,4 +126,21 @@ fn spawn_camera(mut commands: Commands) {
         Collider::default(),
         Sensor,
     ));
+}
+
+fn zoom(
+    mut camera: Query<(&mut Transform, With<Camera3d>)>,
+    mut reader: EventReader<MouseWheel>,
+    time: Res<Time>,
+) {
+    for event in reader.iter() {
+        let (mut transform, _) = camera.single_mut();
+        if event.y == -1.0 {
+            transform.translation.y += time.delta_seconds() * ZOOM_SPEED
+        }
+
+        if event.y == 1.0 {
+            transform.translation.y -= time.delta_seconds() * ZOOM_SPEED
+        }
+    }
 }
